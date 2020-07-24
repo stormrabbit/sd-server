b3172cc81dfca11bd64dba370071f5bc9c65f503
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-07-24 20:22:57 +0800

    时间按照 24 小时格式显示

--- src/events/create-events.dto.ts
+++ src/events/create-events.dto.ts
@@ -3,6 +3,6 @@ export class CreateEventsDto {
     readonly eventStr :string;
     static toResult(dto:CreateEventsDto){
         const date = new Date();
-        return `${date.getTime()}\t\t${moment(date).format('MM-DD hh:mm:ss')}\t\t${dto.eventStr}`;
+        return `${date.getTime()}\t\t${moment(date).format('MM-DD HH:mm:ss')}\t\t${dto.eventStr}`;
     }
 }
