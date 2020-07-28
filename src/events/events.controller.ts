6de0f7476fe505cde41b4b9ecc55c054a3fd731f
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-07-28 21:44:53 +0800

    调整排序

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,17 +1,16 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { CreateEventsDto } from './create-events.dto';
 import {fsTools } from 'eschew-materials';
-import { json } from 'express';
 @Controller('v1/events')
 export class EventsController {
-
+    LIMIT = 20
     @Post()
     async createEvents( @Body() dto:CreateEventsDto) {
         const result = CreateEventsDto.toResult(dto);
         console.log(result);
         const current = await fsTools.readFilePlus('.log');
         const finalResult:Array<String> = Array.from (current.split('\n').filter(str => !!str));
-        if(finalResult.length > 50) {
+        if(finalResult.length > this.LIMIT) {
             finalResult.shift();
         }
         finalResult.push(result);
@@ -31,7 +30,7 @@ export class EventsController {
             }
         });
 
-        return items;
+        return items.reverse();
         // return result.split('\n').filter(str => !!str).map(str => `<p>${str}</p>`).join('');
     }
 }
