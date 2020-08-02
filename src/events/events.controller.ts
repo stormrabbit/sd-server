688a067f7d665f5e0dc25e2dc7574656abbc56cb
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-08-02 23:44:45 +0800

    跨域,未完成

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,21 +1,31 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get,Options } from '@nestjs/common';
 import { CreateEventsDto } from './create-events.dto';
 import {fsTools } from 'eschew-materials';
 @Controller('v1/events')
 export class EventsController {
+    // @Options()
+    // async dealWithOptions() {
+    //     return 'ok'
+    // }
+
     LIMIT = 20
+    // @Post()
+    // async createEvents( @Body() dto:CreateEventsDto) {
+    //     const result = CreateEventsDto.toResult(dto);
+    //     console.log(result);
+    //     const current = await fsTools.readFilePlus('.log');
+    //     const finalResult:Array<String> = Array.from (current.split('\n').filter(str => !!str));
+    //     if(finalResult.length > this.LIMIT) {
+    //         finalResult.shift();
+    //     }
+    //     finalResult.push(result);
+    //     await fsTools.writeFilePlus('.log', finalResult.filter(str => !!str).join('\n'));
+    //     return result;
+    // }
     @Post()
-    async createEvents( @Body() dto:CreateEventsDto) {
-        const result = CreateEventsDto.toResult(dto);
-        console.log(result);
-        const current = await fsTools.readFilePlus('.log');
-        const finalResult:Array<String> = Array.from (current.split('\n').filter(str => !!str));
-        if(finalResult.length > this.LIMIT) {
-            finalResult.shift();
-        }
-        finalResult.push(result);
-        await fsTools.writeFilePlus('.log', finalResult.filter(str => !!str).join('\n'));
-        return result;
+    async createEvents( @Body() dto:any) {
+        console.log(dto);
+        return dto;
     }
 
     @Get()
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,22 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  // const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.use((req, res, next) => {
+    
+    // console.log(req);
+
+    // res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+    res.header('Access-Control-Allow-Credentials',true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
+    res.header("Access-Control-Allow-Headers", "*");
+    res.
+    // res.header("Access-Control-Max-Age", 1728000)
+    next();
+    });
   await app.listen(40439);
 }
 bootstrap();
