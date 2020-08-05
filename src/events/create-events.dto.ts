3041a94149dfeb4b7930e535f3818d8a67327bba
龙骑将杨影枫 <catwindboy@hotmail.com>
2020-08-05 10:22:01 +0800

    fix: 去掉 temp.db

--- src/events/create-events.dto.ts
+++ src/events/create-events.dto.ts
@@ -1,8 +1,4 @@
 // import * as moment from 'moment';
 export class CreateEventsDto {
     readonly eventStr :string;
-    // static toResult(dto:CreateEventsDto){
-    //     const date = new Date();
-    //     return `${date.getTime()}\t\t${moment(date).format('MM-DD HH:mm:ss')}\t\t${dto.eventStr}`;
-    // }
 }
